f6a7a8282207ea9903ff16fcb9c2a480054c5615
Hayden Rear <hayden.rear@gmail.com>
2021-04-11 14:43:59 -0700

    updated to remove everything except name and date and rename file to reflect

--- main.py
+++ main.py
@@ -2,19 +2,49 @@ import os
 import pandas as pd
 from django.utils.text import slugify
 import uuid
+import remove_columns
 
-filespath = '/Users/hayde/IdeaProjects/drools/data/src/main/resources/data/cleaning/'
+datadirectory = '/Users/hayde/IdeaProjects/drools/data/src/main/resources/data/'
+new_dir_folder = 'new_dir/'
+new_dir = datadirectory+new_dir_folder
+stripdirectory = datadirectory+'to_strip/'
+output_final = datadirectory + "final_after/"
 
 def breakexcels():
-    for file in os.listdir(filespath):
+    for file in os.listdir(datadirectory+new_dir_folder):
         if '.xlsx' in file:
             thisfilename = slugify(file.split(".")[0])
-            filepath = filespath+thisfilename+'dir'
-            print(filepath)
-            if os.path.exists(filepath) == False:
-                os.mkdir(filepath)
-            df = pd.read_excel(filespath + file)
-            multi_index_and_recurse(df, file, filepath)
+            new_strip = stripdirectory+thisfilename+"_dir"
+            if not os.path.exists(new_strip):
+                os.mkdir(new_strip)
+            df = pd.read_excel(new_dir + "/" + file)
+            df = delete_unique_and_empty(df)
+            multi_index_and_recurse(df, file, new_strip)
+            strip_columns_and_save()
+
+
+def strip_columns_and_save():
+    for to_strip_dirs in os.listdir(stripdirectory):
+        print(to_strip_dirs)
+        if os.path.isdir(stripdirectory+to_strip_dirs):
+            output_final_stripped_file_directory = output_final + to_strip_dirs
+            print(output_final_stripped_file_directory)
+            if not os.path.exists(output_final_stripped_file_directory):
+                os.mkdir(output_final_stripped_file_directory)
+            for csv in os.listdir(stripdirectory + to_strip_dirs):
+                if '.csv' in csv:
+                    stripped_df = pd.read_csv(stripdirectory + to_strip_dirs + "/" + csv)
+                    col_removal = remove_columns.RemoveColumns(stripped_df,
+                                                               output_final_stripped_file_directory)
+                    col_removal.remove_redundant_columns_and_rename_file()
+
+
+def delete_unique_and_empty(df):
+    cols_to_drop = []
+    for col in df.columns:
+        if len(df[col].unique()) == 1 or len(df[col].unique()) == 0:
+            cols_to_drop.append(col)
+    return df.drop(columns=cols_to_drop)
 
 def get_columns_with_one_unique(df):
     col_with_one_unique = []
@@ -23,15 +53,14 @@ def get_columns_with_one_unique(df):
             col_with_one_unique.append(col)
     return col_with_one_unique
 
-
-def save_to_file(df, filename, filepath):
+def save_to_file(df, filename, strip_dir):
     columns = []
     filename = slugify(filename)
     for col in get_columns_with_one_unique(df):
         filename += slugify(col)
         columns.append(col)
     df=df.drop(columns=columns)
-    df.to_csv(filepath+"/"+slugify(filename+str(uuid.uuid4()))[0:240]+".csv")
+    df.to_csv(strip_dir+"/"+slugify(filename+str(uuid.uuid4()))[0:240]+".csv")
 
 def get_column_with_least_unique(columns, df, date_column, value_column):
     least = len(df)+10
@@ -58,18 +87,17 @@ class tuple:
 def create_index_with_least_unique(df, column, unique_value):
     return df.loc[df.index.get_level_values(column) == unique_value]
 
-def multi_index_and_recurse(df, filename, filepath):
+def multi_index_and_recurse(df, filename, strip_dir):
     thisfile = filename.split(".")[0]
     index = pd.MultiIndex.from_frame(df)
     df.set_index(index, inplace=True)
-    recurse(df, thisfile, filepath)
+    recurse(df, thisfile, strip_dir)
 
 def get_unique_values(df, col):
     return df[col].unique()
 
 def recurse(df, filename, filepath):
     tuple = get_column_with_least_unique(df.columns, df, "Date", "Value")
-    print(df.head())
     if len(tuple.columns) == 0:
         save_to_file(df, filename, filepath)
     else:
@@ -84,4 +112,4 @@ def find_all_unique(dfs):
            final_dfs.append(df)
     return final_dfs
 
-# breakexcels()
+breakexcels()
--- remove_columns.py
+++ remove_columns.py
@@ -1,46 +1,84 @@
 import os
 import pandas as pd
 from django.utils.text import slugify
-
-filespath = '/Users/hayde/IdeaProjects/drools/data/src/main/resources/data/one/'
-
-
-def remove_letters(cols_to_drop, num_letters_remove_each_col):
-    filename = ""
-    for col in cols_to_drop:
-        filename += col[0:len(col) - (num_letters_remove_each_col + 1)]
-    print(filename + " is new filename")
-    return filename
+import numpy as np
 
 
-def remove_redundant_columns_and_rename_file():
-    for file in os.listdir(filespath):
-        if os.path.isdir(filespath + file):
-            for csv in os.listdir(filespath + file):
-                if '.csv' in file:
-                    df = pd.read_csv(filespath + file + "/" + csv)
-                    cols_to_drop = get_column_names_to_drop(df)
-                    num_letters = get_num_letters(cols_to_drop)
-                    num_letters_remove_each_col = 0
-                    if num_letters > 235:
-                        ratio = num_letters / 235
-                        num_letters_to_remove = 235 * (1 / ratio)
-                        num_letters_remove_each_col = num_letters_to_remove / len(cols_to_drop)
-                    filename = remove_letters(cols_to_drop, num_letters_remove_each_col)
-                    print(str(cols_to_drop) + " are the cols to drop")
-                    df = df.drop(columns=cols_to_drop)
-                    df.to_csv(filespath+file+"/"+slugify(filename))
-                    os.remove(filespath+file+"/"+csv)
+class RemoveColumns:
 
-def get_column_names_to_drop(df):
-    columns_to_drop = []
-    for column in df.columns:
-        if len(df[column].unique()) == 1:
-            columns_to_drop.append(column)
-    return columns_to_drop
+    def __init__(self, df, output_filepath):
+        self.df = df
+        self.cols_to_name = []
+        self.num_letters_remove_each_col = 0
+        self.filename = ""
+        self.output_filepath = output_filepath
 
-def get_num_letters(cols_to_drop):
-    sum = 0
-    for col in cols_to_drop:
-        sum += len(col)
-    return sum
+    def remove_letters(self):
+        filename = ""
+        for col in self.cols_to_name:
+            try:
+                col = str(col)
+                filename += col[0:len(col) - (self.num_letters_remove_each_col + 1)]
+            except:
+                continue
+        return filename
+
+    def remove_redundant_columns_and_rename_file(self):
+        self.filename = self.get_filename()
+        self.df = self.df.drop(columns=self.get_cols_to_drop())
+        same_cols = self.duplicate_columns()
+        self.df = self.df.drop(columns=same_cols)
+        self.df.to_csv(self.output_filepath + "/" + slugify(self.filename) + ".csv")
+
+    def get_filename(self):
+        self.cols_to_name = self.get_names_to_add_to_file()
+        num_letters = self.get_num_letters()
+        if num_letters > 235:
+            ratio = num_letters / 235
+            num_letters_to_remove = 235 * (1 / ratio)
+            self.num_letters_remove_each_col = num_letters_to_remove / len(self.cols_to_name)
+        filename = self.remove_letters()
+        return filename
+
+    def duplicate_columns(self):
+        groups = self.df.columns.to_series().groupby(self.df.dtypes).groups
+        dups = []
+
+        for t, v in groups.items():
+            cs = self.df[v].columns
+            vs = self.df[v]
+            lcs = len(cs)
+
+            for i in range(lcs):
+                ia = vs.iloc[:, i].values
+                for j in range(i + 1, lcs):
+                    ja = vs.iloc[:, j].values
+                    if np.array_equal(ia, ja):
+                        dups.append(cs[j])
+                        break
+        return dups
+
+    def get_names_to_add_to_file(self):
+        name_to_add = []
+        for column in self.df.columns:
+            if len(self.df[column].unique()) == 1:
+                name_to_add.append(self.df[column].unique()[0])
+        return name_to_add
+
+    def get_cols_to_drop(self):
+        columns_to_drop = []
+        for column in self.df.columns:
+            if len(self.df[column].unique()) == 1:
+                columns_to_drop.append(column)
+        return columns_to_drop
+
+    def get_num_letters(self):
+        sum = 0
+        for col in self.cols_to_name:
+            try:
+                col = str(col)
+            except:
+                sum += 3
+                continue
+            sum += len(col)
+        return sum
